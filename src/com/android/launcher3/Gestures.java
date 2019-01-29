e3a1e75d6bc159b6988602427eb8157a6dbfcf19
Harsh Shandilya <msfjarvis@gmail.com>
2019-01-29 16:13:22 +0530

    Gestures: Commonize duplicate code
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>
    Change-Id: I24bd5a1a3fbddf359179961b3368c83df1643e11

--- src/com/android/launcher3/Gestures.java
+++ src/com/android/launcher3/Gestures.java
@@ -118,28 +118,13 @@ public class Gestures extends SettingsActivity
         public boolean onPreferenceChange(Preference preference, final Object newValue) {
             switch (preference.getKey()) {
                 case KEY_HOMESCREEN_DT_GESTURES:
-                    String dtGestureValue = (String) newValue;
-                    getDevicePrefs(mContext).edit().putString(KEY_HOMESCREEN_DT_GESTURES,
-                        dtGestureValue).commit();
-                    mDoubleTapGestures.setValue(dtGestureValue);
-                    mDoubleTapGestures.setSummary(mDoubleTapGestures.getEntry());
-                    LauncherAppState.getInstanceNoCreate().setNeedsRestart();
+                    handlePreferenceChange(mDoubleTapGestures, KEY_HOMESCREEN_DT_GESTURES, (String) newValue, true);
                     break;
                 case KEY_HOMESCREEN_SWIPE_DOWN_GESTURES:
-                    String swipeDownGestureValue = (String) newValue;
-                    getDevicePrefs(mContext).edit().putString(KEY_HOMESCREEN_SWIPE_DOWN_GESTURES,
-                        swipeDownGestureValue).commit();
-                    mSwipeDownGestures.setValue(swipeDownGestureValue);
-                    mSwipeDownGestures.setSummary(mSwipeDownGestures.getEntry());
-                    LauncherAppState.getInstanceNoCreate().setNeedsRestart();
+                    handlePreferenceChange(mSwipeDownGestures, KEY_HOMESCREEN_SWIPE_DOWN_GESTURES, (String) newValue, true);
                     break;
                 case KEY_HOMESCREEN_SWIPE_UP_GESTURES:
-                    String swipeUpGestureValue = (String) newValue;
-                    getDevicePrefs(mContext).edit().putString(KEY_HOMESCREEN_SWIPE_UP_GESTURES,
-                        swipeUpGestureValue).commit();
-                    mSwipeUpGestures.setValue(swipeUpGestureValue);
-                    mSwipeUpGestures.setSummary(mSwipeUpGestures.getEntry());
-                    LauncherAppState.getInstanceNoCreate().setNeedsRestart();
+                    handlePreferenceChange(mSwipeUpGestures, KEY_HOMESCREEN_SWIPE_UP_GESTURES, (String) newValue, true);
                     break;
             }
             return false;
@@ -148,6 +133,14 @@ public class Gestures extends SettingsActivity
         @Override
         public boolean onPreferenceClick(Preference preference) {
             return false;
+        }
+
+        private void handlePreferenceChange(final ListPreference pref, final String prefKey,
+                                            final String value, final boolean needsRestart) {
+            getDevicePrefs(mContext).edit().putString(prefKey, value).commit();
+            pref.setValue(value);
+            pref.setSummary(pref.getEntry());
+            if (needsRestart) LauncherAppState.getInstanceNoCreate().setNeedsRestart();
         }
     }
 
